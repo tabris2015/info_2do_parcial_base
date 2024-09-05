63acc55c638866e883efe8f7cfbd04799f234055
Jose Laruta <pepe@Joses-MacBook-Pro.local>
2024-09-05 07:44:48 -0400

    base

---
+++ scripts/top_ui.gd
@@ -0,0 +1,7 @@
+extends TextureRect
+
+@onready var score_label = $MarginContainer/HBoxContainer/score_label
+@onready var counter_label = $MarginContainer/HBoxContainer/counter_label
+
+var current_score = 0
+var current_count = 0
